d86889d71803afa0ecd63730d077df23e274be3f
Aryandhumal27 <aryandhumal2710@gmail.com>
2025-11-24 00:14:47 +0530

    Add files via upload

---
+++ Assignments/Assignment_31/program31_4.java
@@ -0,0 +1,66 @@
+// 4. Accept number of rows and number of columns from user and display below pattern.
+
+/*
+    Input : iRow = 6
+            iCol = 6
+
+    Output :    *   *   *   *   *   *
+                *   *               *
+                *       *           *
+                *           *       *
+                *               *   *
+                *   *   *   *   *   *
+*/
+import java.util.Scanner;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        if(iRow != iCol)
+        {
+            System.out.println("Invalid Output");
+            System.out.println("You have to give same number of rows and columns as input.");
+            return;
+        }
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if((i == j) || (i == 1) || ( i == iRow) || (j == 1) || (j == iCol))
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                    System.out.print(" \t");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+class program31_4
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+
+        sobj.close();
+    }
+}
